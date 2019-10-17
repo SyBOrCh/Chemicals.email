8c726c34d7da72b7df980a46ebea87be777bd0e5
John Braun <johnbraun@pm.me>
2019-10-17 15:39:27 +0200

    WIP

--- resources/views/emails/searchresults.blade.php
+++ resources/views/emails/searchresults.blade.php
@@ -1,14 +1,17 @@
-Your search results are ready:
+Your search results from the {{ ucfirst($mail->group) }} database:
 
 <ul>
 @foreach ($results as $keyword => $results)
 	<li>
-		<strong> {{ $keyword}} </strong>
+		"<strong> {{ $keyword}} </strong>":
 		<ul>
 			@foreach ($results as $result)
 			<li>
-				Name: {{ $result['name'] }} <br>
-				Location: {{ $result['location'] }}
+                Name: <a href="http://database.syborchserver.nl/chemical/{{ $result['id'] }}">{{ $result['name'] }}</a> <br>
+                Quantity: {{ $result['quantity'] }} <br>
+				Location: {{ $result['location'] }} . {{ $result['cabinet'] }} . {{ $result['number'] }} <br>
+                MW: {{ $result['molweight'] }} <br>
+                density: {{ $result['density'] }}
 			</li>
 			@endforeach
 		</ul>
